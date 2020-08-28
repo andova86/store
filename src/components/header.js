ff360d42d264cb3ee0799022af3549399a8b058f
andova86 <andova86@gmail.com>
2020-08-28 18:54:09 -0400

    Primer Commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,21 @@
-import { Link } from "gatsby"
+
 import PropTypes from "prop-types"
 import React from "react"
+import NavBar from '../components/header/NavBar'
+
+
 
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
+  title={ siteTitle }
+    
   >
     <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+     
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+     <NavBar />
+
+    
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,31 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { Container, Grid } from "@material-ui/core"
+import ProductList from '../components/Card/ProductList'
 
 const IndexPage = () => (
   <Layout>
+    <Container style={{paddingTop:'30px', paddingBottom:'30px'}}>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+
+ 
+
+     
+
+    <ProductList/>
+   {/*  <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    
+
+
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
+
+    </Container>
+
+   
   </Layout>
 )
 
